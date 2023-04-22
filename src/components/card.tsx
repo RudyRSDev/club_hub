80ec2de20e029fbe13a3c7e27c7b39189693e3d2
MekhiHart <mekhihartdelacruz@gmail.com>
2023-04-21 22:03:20 -0700

    added rendered buttons based off clubs.ts

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,15 +1,16 @@
 // import { FaFacebookF, FaDiscord } from "react-icons/fa";
 
 const Card = ({Title, Descpt, Links}:any) => {
+
+    const createButtons = Links.map((link:any, index:Number) => <button className="btn btn-primary"><a href={link.url} target="_blank" >{link.name}</a></button> )
     return (
-        <div className="card card-compact w-96 bg-base-200 shadow-xl">
+        <div  className="card card-compact w-96 bg-base-200 shadow-xl">
             <figure><img src="https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg" alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{Title}</h2>
                 <p>{Descpt}</p>
                 <div className="card-actions justify-end">
-                    {/* Map Links to buttons */}
-                    <button className="btn btn-primary">Test</button>
+                    {createButtons}
                 </div>
             </div>
         </div>
